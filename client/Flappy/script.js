0be7f2cd83b9f68ea08a8f222ef394bcce5f0dcb
wyatthickman <89927529+wyatthickman@users.noreply.github.com>
2021-09-22 21:56:21 -0400

    Flappy

---
+++ client/Flappy/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    const bird = document.querySelector('.bird')
+    const gameDisplay = document.querySelector('.game-container')
+    const ground = document.querySelector('.ground')
+
+    let birdLeft=220
+    let birdBottom = 100
+    let gravity = 2
+
+    function startGame(){
+        birdBottom -= gravity
+        bird.style.bottom= birdBottom + "px"
+        bird.style.left= birdLeft + "px"
+    }
+    let timerID= setInterval(startGame, 20)
+
+    function jump(){
+        if (birdBottom < 500) birdBottom +=50
+        bird.style.bottom = birdBottom + 'px'
+        console.log(birdBottom)
+
+    }
+    document.addEventListener('keyup', jump)
+
+
+
+
+}
+
+)
